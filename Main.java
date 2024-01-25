eaea798ccd2244fe7571eedb1778fe6e13aab132
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2024-01-25 21:14:54 +0000

    spaced out

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main{
   public static void main(String[] args){
       System.out.println("hello");
       System.out.println("hello");
+
+
+      
       System.out.println(quadrant(3,10));
       System.out.println(quadrant(-3,10));
       System.out.println(quadrant(3,-10));
